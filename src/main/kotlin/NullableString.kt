f47706760612c89d3d62fc1e23beee1c89fcfa88
anirudhkmitl <62011088@kmitl.ac.th>
2021-12-28 10:07:09 +0700

    [ADDED] When{} block

--- src/main/kotlin/NullableString.kt
+++ src/main/kotlin/NullableString.kt
@@ -25,6 +25,6 @@ fun main() {
         print("hi")
     }
 
-
+    
 }
 
---
+++ src/main/kotlin/WhenStatements.kt
@@ -0,0 +1,15 @@
+/*
+When{} block is essentially an advanced form of the switch-case statement known from Java.
+
+In Kotlin, if a matching case is found then only the code in the respective case block is executed and execution continues
+with the next statement after the when block. This essentially means that no break statements are needed at the end of each
+case block.
+ */
+
+var variable: String? = null
+fun main() {
+    when(variable){
+        null -> println("Hello")
+        else -> println(variable)
+    }
+}
