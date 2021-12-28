b33b9e55d4cbe62ae5a24c2f0be08a4184b6b120
anirudhkmitl <62011088@kmitl.ac.th>
2021-12-28 09:40:58 +0700

    Hello World

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+
+//Hello World
+/*
+fun main(args: Array<String>) {
+    println("Hello World!")
+    println("Program arguments: ${args.joinToString()}")
+}
+*/
+
+//Variables
+fun main() {
+    /*
+    * There are two types of variables
+    * VAR - these variables are mutable and can be reassigned later.
+    * VAL - these are local read only variables - can be defined only once.
+    * */
+    var name: String = "Nate"
+    name = "Anirudh"
+
+    println(name)
+}
+
